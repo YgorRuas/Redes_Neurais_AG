987582ed241866d048440cd65cf87047eda4e337
Ygor Fagundes Ruas <ygor220066@ilum.cnpem.br>
2023-03-09 19:51:22 -0300

    adicionando atividades 1 e 2

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,8 @@
- 
+def funcao_objetivo_cb(individuo):
+    """Computa a função objetivo no problema das caixas binárias.
+    Args:
+        indivíduo: lista contendo os genes das caixas binárias.
+    Return:
+        A soma dos genes do indivíduo.
+    """
+    return sum(individuo) 
